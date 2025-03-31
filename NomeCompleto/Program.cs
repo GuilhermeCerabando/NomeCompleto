e1be9fbf45a26f4314a3b800597e88e59794cfe4
GuilhermeCerabando <cerabando@gmail.com>
2025-03-31 14:41:31 -0300

    Exercício Completo

--- NomeCompleto/Program.cs
+++ NomeCompleto/Program.cs
@@ -1,2 +1,13 @@
 ﻿
-Console.WriteLine("Hello, World!");
+String nome,sobrenome;
+
+Console.Write("Digite Seu Nome: ");
+nome = Console.ReadLine();
+
+Console.Write("Digite Seu Sobrenome:  ");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine("Nome Completo: " + nome + " " + sobrenome);
+Console.WriteLine("Nome de Catálogo: " + sobrenome.ToUpper() + ", " + nome);
+
+
